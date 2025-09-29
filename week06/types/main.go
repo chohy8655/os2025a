7bfef23c26a6098d4e2fd185ce253010486ac2ed
cho <chohy8655@naver.com>
2025-09-29 11:49:09 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
 )
 
 func main() {
-	fmt.Println(math.Round(2.71))
-	fmt.Println(strings.Title("head first go"))
+	//var name = "kim inha"
+
+	//var name string
+	//name = "kim inha"   //변수선언방법3가지
+
+	name := "kim inha" //:=이건 선언할때 쓰는것 , 문자열변수 선언했는데 거기다 다른거 넣으면 에러뜬다
+	//:= 와 = 구분하기
+
+	// name = 2.71은 float64   따라서 var name float32 로 선언
+
+	fmt.Println(name)
 
 }
