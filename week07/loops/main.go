34173f9e6aa52b0f9a18fb92e94c3da6e729a004
cho <chohy8655@naver.com>
2025-10-13 10:31:58 +0900

    v0.1 loopx/casting

---
+++ week07/loops/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var length float64 = 1.2
+	var width int = 2
+	fmt.Println("면적은", int(length)*width)
+	fmt.Println("length > width?", int(length) > width)
+	fmt.Println("length > width?", int(length) > width)
+	fmt.Println("origin", width)
+	fmt.Println("origin", width)
+}
