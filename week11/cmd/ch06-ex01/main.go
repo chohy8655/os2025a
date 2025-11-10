5891a907a535c09f7518dd54ddb582703ed27e1f
cho <chohy8655@naver.com>
2025-11-10 11:28:10 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
 
-	subjects := []string{"Go", "", "python"}
-
+	subjects := []string{"Go", "javascript", "python", "linux"}
+	subjectsSlice := subjects[1:3]
 	for _, susubject := range subjects {
 		fmt.Println(susubject)
 
+	}
+	fmt.Println("**************")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 
 }
